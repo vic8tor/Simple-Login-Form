fdd227916b0ad0e90e54f0a0fa12ad953a857950
vic8tor <timongrinni@gmail.com>
2021-12-14 16:34:49 +0300

    Add extension (alert)

--- Simple Login Form/ViewController.swift
+++ Simple Login Form/ViewController.swift
@@ -9,10 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var userForgotButton: UIButton!
+    @IBOutlet weak var passwordForgotButton: UIButton!
+    private let password = "Swift"
+    let userName = "Developer"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     
     }
     
+    @IBAction func loginAction() {
+    }
+    
+    
+    
+    @IBAction func userForgotAction() {
+        showAlert(title: "Attension!", message: "Your login is \(userName)")
+    }
 }
-
+// MARK: - Private Methods
+extension ViewController {
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+        }
+}
