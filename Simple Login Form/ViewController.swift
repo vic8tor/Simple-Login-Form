50762b5f62894504cadad4595af04773b52922e5
vic8tor <timongrinni@gmail.com>
2021-12-14 13:57:26 +0300

    Initial Commit

---
+++ Simple Login Form/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple Login Form
+//
+//  Created by Victor on 14.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
