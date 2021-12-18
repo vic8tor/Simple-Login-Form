62cce6238cd2bf83a6519a11eab205585b7b229b
vic8tor <timongrinni@gmail.com>
2021-12-18 17:44:34 +0300

    Configured the segmentControl and added a gradient

--- Simple Login Form/ViewControllers/ProfileViewController.swift
+++ Simple Login Form/ViewControllers/ProfileViewController.swift
@@ -9,10 +9,54 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var fullnameLabel: UILabel!
+    @IBOutlet weak var professionInHeaderLabel: UILabel!
+    @IBOutlet weak var cityInHeaderLabel: UILabel!
+    
+    @IBOutlet weak var educationStackView: UIStackView!
+    @IBOutlet weak var experienceStackView: UIStackView!
+    
+    //MARK: - Public Proporeties
+    var fullname: String!
+    var professionInHead: String!
+    var cityInHead: String!
+    
+    // MARK: - Life Cycles Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        grafient()
+        
+        experienceStackView.isHidden = true
+        educationStackView.isHidden = false
         
     }
 
+    @IBAction func changeSegmentControlActions(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            experienceStackView.isHidden = true
+            educationStackView.isHidden = false
+        case 1:
+            experienceStackView.isHidden = false
+            educationStackView.isHidden = true
+        default:
+            experienceStackView.isHidden = true
+            educationStackView.isHidden = true
+        }
+        
+    }
+}
+
+
+extension ProfileViewController {
+    private func grafient() -> Void {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [
+            UIColor.systemRed.cgColor,
+            UIColor.systemPurple.cgColor,
+            UIColor.systemBlue.cgColor
+        ]
+        view.layer.insertSublayer(gradientLayer, at: 0)
+    }
 }
